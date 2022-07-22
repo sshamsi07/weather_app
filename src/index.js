9fea9648432f9cb0f7bbd77d330d905d1a5335fc
ssarah <sarah1.shamsi@gmail.com>
2022-07-22 13:41:24 -0700

    cleaned

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router } from 'react-router-dom';
 import './styling.css';
 import App from './components/App';
 
