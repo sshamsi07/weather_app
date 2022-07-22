9abfe2b43f06a047d18663046cccb7a97d3a75b8
ssarah <sarah1.shamsi@gmail.com>
2022-07-22 13:40:18 -0700

    updated

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,9 @@
 import React from'react'
-
 import CurrentWeather from "./CurrentWeather";
-
-import { Route } from 'react-router-dom';
 
 class App extends React.Component{
   render(){
     return(<div className={'app'}>
-
-            {/*<Route path='/' component={CurrentWeather}/>*/}
         <CurrentWeather/>
 
     </div>)
